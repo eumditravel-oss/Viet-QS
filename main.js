4af6587425f1211856d752a7d0013510332332b2
eumditravel-oss <eumditravel@gmail.com>
2025-12-30 10:53:54 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-document.querySelectorAll(".menu button").forEach(btn => {
-  btn.addEventListener("click", () => {
-    const part = btn.dataset.part;
-    location.href = `guide.html?part=${part}`;
-  });
+document.addEventListener("click", (e) => {
+  const btn = e.target.closest("[data-cat]");
+  if (!btn) return;
+
+  const cat = btn.getAttribute("data-cat");
+  location.href = `guide.html?cat=${encodeURIComponent(cat)}`;
 });
